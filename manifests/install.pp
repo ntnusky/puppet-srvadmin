84a42782e8754bd8eea6f2a5d92f57c066fd45e0
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-06-15 10:41:19 +0200

    wrong parameter. Assumption is the mother of all fsckups

--- manifests/install.pp
+++ manifests/install.pp
@@ -32,7 +32,7 @@ class srvadmin::install {
 
   file { '/usr/local/bin/racadm':
     ensure  => 'link',
-    source  => '/opt/dell/srvadmin/sbin/racadm',
+    target  => '/opt/dell/srvadmin/sbin/racadm',
     require => Package['srvadmin-all'],
   }
 }
